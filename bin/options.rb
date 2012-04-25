2aab254f83d4052b3828ac6ce3d49b8828b00cf9
Kevin Raoofi <nullindev@gmail.com>
2012-04-25 17:25:17 -0400

    Added support for setting config file on option parser dummy program

--- bin/options.rb
+++ bin/options.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/ruby
+# dummy program to get shell options sorted out
 require 'optparse'
 options ={}
 OptionParser.new do |opts|
 	opts.banner = "Usage: deadbot [options]"
-	opts.on("-d","--[no-]debug","Output debug messages to STDOUT") do |o|
-		options[:debug] = o
+	opts.on("-d","--[no-]verbose","Output debug messages to STDOUT") do |o|
+		options[:verbose] = o
+	end
+	opts.on("-c","--config FILE","Uses a separate YAML file in place of ./config.yaml") do |o|
+		options[:config] = o
 	end
 end.parse!
 p options
